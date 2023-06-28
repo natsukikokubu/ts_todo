e882fbb0d9ea0b720c6846da3a2be49cfbb0e013
natsukikokubu <natsukikokubu3@gmail.com>
2023-06-28 22:26:55 +0900

    css修正

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,23 +1,38 @@
-const handleClick = (arg: string | number | boolean) => {
-  if (typeof arg === "string") {
-    alert("文字列ボタンがクリックされました");
-  } else if (typeof arg === "number") {
-    alert("数字ボタンがクリックされました");
-  } else if (typeof arg === "boolean") {
-    alert("真偽値ボタンがクリックされました");
+/*const addTask = (event: SubmitEvent) => {
+  event.preventDefault();
+  const container = document.getElementById("task-container");
+  const taskList = document.getElementById("task-list");
+  const input = document.getElementById("taskInput") as HTMLInputElement;
+  //const inputValue = form.elements.namedItem("task") as HTMLInputElement;
+  const taskInputTemplate = `<label><input class="task" type="checkbox">${input.value}</input></label>`;
+  //if (container) {
+  taskList.insertAdjacentHTML("beforeend", taskInputTemplate);
+  //}
+  input.value = "";
+};*/
+
+const container = document.getElementById("task-container");
+const taskList = document.getElementById("task-list");
+
+const addTask = (event: Event) => {
+  event.preventDefault();
+
+  const input = document.getElementById("taskInput") as HTMLInputElement;
+  const taskName = input.value;
+
+  if (taskName.trim() !== "") {
+    const taskItem = document.createElement("li");
+    taskItem.innerHTML = `<label><input class="task" type="checkbox">${taskName}</label>`;
+
+    if (taskList) {
+      taskList.appendChild(taskItem);
+    }
+
+    input.value = "";
   }
 };
 
-const form = document.getElementById("sample-form");
-
-// formが取得できるかは不明なのでoptional
-form?.addEventListener("submit", (e) => {
-  // submitイベントをキャンセルすることで、ページがリフレッシュされることを防ぐ
-  e.preventDefault();
-
-  // FormDataインスタンスを生成して、formの中身を取得する
-  // e.targetは必ずHTMLFormElementなのでassertionする
-  const formData = new FormData(e.target as HTMLFormElement);
-  const todo = formData.get("todo");
-  console.log(todo);
-});
+const form = document.querySelector("form");
+if (form) {
+  form.addEventListener("submit", addTask);
+}
